9bf65923917115303941a7fe1019ade0ac85a0b6
Rustam Kuzyaev <xpyct405@yandex.ru>
2023-06-15 23:42:56 +0300

    feat: add autoclick for generation password, add copy function with copy to clipboard and notification by the click on password

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let numsAbc = document.querySelector('#nums-abc');
 
 let pass = document.createElement('div');
 pass.classList.add('pass');
-document.body.appendChild(pass);
+document.querySelector('.block').appendChild(pass);
 
 document.querySelector('button').addEventListener('click', () => {
 
@@ -21,12 +21,12 @@ document.querySelector('button').addEventListener('click', () => {
 
     if (numsAbc.checked == false) {
         for (let i = 0; i < numValue.value; i++) {
-            // Получение рандомного символа из строки array и добавление к паролю из предыдущего цикла
+            // Получение рандомного символа из строки numsArray и добавление к паролю из предыдущего цикла
             password += numsArray.charAt(Math.floor(Math.random() * numsArray.length));
         }
     } else {
         for (let i = 0; i < numValue.value; i++) {
-            // Получение рандомного символа из строки array и добавление к паролю из предыдущего цикла
+            // Получение рандомного символа из строки numsAbcArray и добавление к паролю из предыдущего цикла
             password += numsAbcArray.charAt(Math.floor(Math.random() * numsAbcArray.length));
         }
     }
@@ -35,4 +35,27 @@ document.querySelector('button').addEventListener('click', () => {
     pass.innerText = password;
 
     return password
+})
+
+// Чтобы по-дефолту уже был пароль при загрузке страницы
+document.querySelector('button').click();
+
+// При клике по pass копируем содержимое
+pass.addEventListener('click', () => {
+    // Создаём новый элемент
+    let copied = document.createElement('div');
+    copied.classList.add('copied')
+    document.querySelector('.block').appendChild(copied)
+    copied.innerText = 'Copied!'
+
+    // Добавляем класс
+    copied.classList.add('active')
+
+    // Удаляем элемент через 1.3с
+    setTimeout(() => {
+        copied.remove()
+    }, 1300)
+
+    // Метод ниже копирует содержимое pass.innerText в буфер обмена
+    navigator.clipboard.writeText(pass.innerText)
 })
